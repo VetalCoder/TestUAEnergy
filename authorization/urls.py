1f79a4e00d2ba95ce72ce080f86399516f8ba48d
VetalCoder <venturanse@gmail.com>
2019-07-12 01:21:30 +0300

    Add styles for login and signup

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'authorization'
 
 urlpatterns = [
-    path('', views.Login.as_view(), name='login'),
+    path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
