996deda3e151364c12abd63dc537ffa321bdea24
VetalCoder <venturanse@gmail.com>
2019-07-11 13:21:32 +0300

    Added slyles for base.html

--- TestUAEnergy/settings.py
+++ TestUAEnergy/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Login/LogOut links
 LOGIN_REDIRECT_URL = reverse_lazy('csv_worker:home')
 LOGOUT_REDIRECT_URL = reverse_lazy('home')
--- authorization/forms.py
+++ authorization/forms.py
@@ -6,5 +6,5 @@ class UserEnergyForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
         if not user.is_active:
             raise ValidationError("Access denied")
-        if user.username != 'test5' and user.username != 'uaenergy':  # TODO: delete test case
+        if user.username != 'test' and user.username != 'uaenergy':  # TODO: delete test case
             raise ValidationError("You can't login with this account.")
